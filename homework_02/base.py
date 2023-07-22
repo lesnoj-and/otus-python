8bf8203d7772adaecb094de3d8d7be7f574fcb5b
Андрей <agsharonov@sberautotech.ru>
2023-07-22 11:43:47 +0300

    linter

--- homework_02/base.py
+++ homework_02/base.py
@@ -3,14 +3,12 @@ from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    def __init__(self, weight = 0, fuel =0 , fuel_consumption = 0) -> None:
-        #super().__init__()
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0) -> None:
         self.weight = weight
         self.fuel = fuel
-        self.fuel_consumption = fuel_consumption 
+        self.fuel_consumption = fuel_consumption
         self.started = False
 
-    
     def start(self):
         if not self.started:
             if self.fuel > 0:
@@ -19,11 +17,9 @@ class Vehicle(ABC):
             else:
                 raise LowFuelError
 
-
     def move(self, dist):
         need = self.fuel_consumption * dist
         if need > self.fuel:
             raise NotEnoughFuel
         else:
             self.fuel -= need
- 
--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -5,6 +5,7 @@
 - CargoOverload
 """
 
+
 class LowFuelError(Exception):
     def __init__(self):
         super().__init__()
@@ -14,12 +15,15 @@ class NotEnoughFuel(Exception):
     def __init__(self):
         super().__init__()
 
+
 class CargoOverload(Exception):
     def __init__(self, message):
         self.message = message
+
     def __str__(self):
         return self.message
-    
+
+
 class VehicleNotStartedError(Exception):
     def __init__(self):
         super().__init__()
